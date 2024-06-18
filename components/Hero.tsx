a8253793eda5702ae21ae46760f76b927cdbad92
marywebby <marywebby1999@gmail.com>
2024-06-18 15:53:09 -0500

    imported theme provider for dark mode

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -14,6 +14,12 @@ const Hero = () => {
         {/* Radial gradient for the container to give a faded look */}
         <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
         
+        <div className="flex justify-center">
+          <div className="max-w-[89vw]">
+            dynamic wbe magic with next.js
+          </div>
+        </div>
+
       </div>
     </div>
   )
