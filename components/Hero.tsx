4f88bc83338381ae9f4d78f733e82ce22412885f
marywebby <marywebby1999@gmail.com>
2024-06-18 15:58:37 -0500

    added a smaller grid

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -10,10 +10,10 @@ const Hero = () => {
             <Spotlight className="-top-28 -left-80 h-[80-vh] v-[50-vw]" fill='blue'/>
         </div>
 
-        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.3] bg-grid-black/[0.2] relative flex items-center justify-center">
+        <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-small-white/[0.3] bg-grid-small-black/[0.2] relative flex items-center justify-center">
         {/* Radial gradient for the container to give a faded look */}
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
-        
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]" />
+
         <div className="flex justify-center">
           <div className="max-w-[89vw]">
             dynamic wbe magic with next.js
