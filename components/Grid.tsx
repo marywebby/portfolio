4d8a6fd7f9005150d9031ffd327362265c4a466b
marywebby <marywebby1999@gmail.com>
2024-06-20 16:02:52 -0500

    bento grid customized with properties

--- components/Grid.tsx
+++ components/Grid.tsx
@@ -1,8 +1,20 @@
-import React from 'react'
+import { BentoGrid, BentoGridItem } from './ui/BentoGrid'
+import { title } from 'process'
 
 const Grid = () => {
   return (
-    <div>Grid</div>
+    <section id="about">
+      <BentoGrid>
+        {[{ title: "title1", description: "Desc1", id:1}].map((item) => (
+            <BentoGridItem
+                id={item.id}
+                key={item.id} 
+                title={item.title} 
+                description={item.description}
+            />
+        ))}
+      </BentoGrid>
+    </section>
   )
 }
 
